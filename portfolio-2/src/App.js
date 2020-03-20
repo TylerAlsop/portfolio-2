e4fc6b1006213d40de0e0545311621c54affc06c
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-19 18:03:36 -0600

    Replaced default return in App.js with a very basic skeleton for the app.

--- portfolio-2/src/App.js
+++ portfolio-2/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1 className="app-heading">Tyler Alsop</h1>
+        <h2 className="app-subheading">Web Developer</h2>
       </header>
+      <div className="deployed-projects">
+        <h2><a href="https://zeit.co/tyleralsop/react-american-football-scoreboard">American Football Scoreboard</a></h2>
+      </div>
+
     </div>
   );
 }
